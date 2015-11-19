472dc618f701cb32cd22d6402ff4ab2e03c9df85
diogo <diogo@gmail.com>
2015-11-19 21:25:32 -0200

    Fixing the files

---
+++ page6.php
@@ -0,0 +1,11 @@
+		<section id="steps-intro" class="steps-intro">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h1>Etapas da pesquisa</h1>
+						<p>O projeto de pesquisa está dividido em 8 etapas. As duas primeiras duram cerca de 2 horas. A terceira etapa dura 15 minutos. A quarta etapa necessita de pelo menos 1 hora por semana de cada programador. As demais etapas duram de 2 a 3 horas. </p>
+						<p><a href="#steps" class="arrow-btn">Veja todos os passos! <i class="fa fa-long-arrow-right"></i></a></p>
+					</div>
+				</div>
+			</div>
+		</section>
---
+++ page8.php
@@ -0,0 +1,47 @@
+<!--
+		<section class="download" id="download">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center wp4">
+						<h1>Seen Enough?</h1>
+						<a href="http://tympanus.net/codrops/?p=22554" class="download-btn">Download! <i class="fa fa-download"></i></a>
+					</div>
+				</div>
+			</div>
+		</section>
+-->
+		<section class="contact" id="contact">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center wp4">
+						<h1>Participe do Projeto de Pesquisa</h1>
+<form method="post">
+<div class="form-group">
+<label for="nome">Nome:</label>
+<br />
+<input type="text" id="nome" name="nome" placeholder="Nome" class="form-control"/>
+<br />
+<label for="empresa">Nome da Empresa/Grupo:</label>
+<br />
+<input type="text" id="empresa" name="empresa" placeholder="Empresa/Grupo" class="form-control"/>
+<br />
+<label for="email">E-mail:</label>
+<br />
+<input type="email" id="email" name="email" placeholder="E-mail" class="form-control"/>
+<br />
+<label for="fone">Telefone:</label>
+<br />
+<input type="fone" id="fone" name="fone" placeholder="(XX) XXXX-XXXX" class="form-control"/>
+<br />
+<label for="msg">Mensagem:</label>
+<br />
+<textarea id="msg" name="msg" placeholder="" class="form-control" rows="5"></textarea>
+<br />
+<br />
+<input type="submit" value="Participar" class="btn btn-default btn-lg" />
+</div>
+</form>						
+					</div>
+				</div>
+			</div>
+		</section>
