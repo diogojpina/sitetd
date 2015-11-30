48d38d9d85b26c4dc981b7c8b7ec5d1053073bbf
Diogo Pina <diogojpina@gmail.com>
2015-11-30 18:24:39 -0200

    Alter the images sort of the step page.

--- page7.php
+++ page7.php
@@ -17,12 +17,25 @@
 						</li>
 						<li>
 							<figure>
-								<img src="img/02-screenshot.jpg" alt="Screenshot 01">
+								<img src="img/07-screenshot.jpg" alt="Screenshot 01">
 								<figcaption>
 								<div class="caption-content">
-									<a href="img/large/02.jpg" class="single_image">
+									<a href="img/large/07.jpg" class="single_image">
 										<i class="fa fa-search"></i><br>
 										<p>Palestra sobre dívida técnica e ferramentas</p>
+									</a>
+								</div>
+								</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="img/06-screenshot.jpg" alt="Screenshot 01">
+								<figcaption>
+								<div class="caption-content">
+									<a href="img/large/06.jpg" class="single_image">
+										<i class="fa fa-search"></i><br>
+										<p>Questionário de caracterização de equipe</p>
 									</a>
 								</div>
 								</figcaption>
@@ -34,19 +47,6 @@
 								<figcaption>
 								<div class="caption-content">
 									<a href="img/large/03.jpg" class="single_image">
-										<i class="fa fa-search"></i><br>
-										<p>Questionário de caracterização de equipe</p>
-									</a>
-								</div>
-								</figcaption>
-							</figure>
-						</li>
-						<li>
-							<figure>
-								<img src="img/04-screenshot.jpg" alt="Screenshot 01">
-								<figcaption>
-								<div class="caption-content">
-									<a href="img/large/04.jpg" class="single_image">
 										<i class="fa fa-search"></i><br>
 										<p>Pagamento de itens de dívida técnica</p>
 									</a>
@@ -60,38 +60,12 @@
 					<ul class="grid">
 						<li>
 							<figure>
-								<img src="img/05-screenshot.jpg" alt="Screenshot 01">
+								<img src="img/02-screenshot.jpg" alt="Screenshot 01">
 								<figcaption>
 								<div class="caption-content">
-									<a href="img/large/05.jpg" class="single_image">
+									<a href="img/large/02.jpg" class="single_image">
 										<i class="fa fa-search"></i><br>
 										<p>Atividade em grupo 1</p>
-									</a>
-								</div>
-								</figcaption>
-							</figure>
-						</li>
-						<li>
-							<figure>
-								<img src="img/06-screenshot.jpg" alt="Screenshot 01">
-								<figcaption>
-								<div class="caption-content">
-									<a href="img/large/06.jpg" class="single_image">
-										<i class="fa fa-search"></i><br>
-										<p>Atividade em grupo 2</p>
-									</a>
-								</div>
-								</figcaption>
-							</figure>
-						</li>
-						<li>
-							<figure>
-								<img src="img/07-screenshot.jpg" alt="Screenshot 01">
-								<figcaption>
-								<div class="caption-content">
-									<a href="img/large/07.jpg" class="single_image">
-										<i class="fa fa-search"></i><br>
-										<p>Entrevista individual com cada programador</p>
 									</a>
 								</div>
 								</figcaption>
@@ -103,6 +77,32 @@
 								<figcaption>
 								<div class="caption-content">
 									<a href="img/large/08.jpg" class="single_image">
+										<i class="fa fa-search"></i><br>
+										<p>Atividade em grupo 2</p>
+									</a>
+								</div>
+								</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="img/04-screenshot.jpg" alt="Screenshot 01">
+								<figcaption>
+								<div class="caption-content">
+									<a href="img/large/04.jpg" class="single_image">
+										<i class="fa fa-search"></i><br>
+										<p>Entrevista individual com cada programador</p>
+									</a>
+								</div>
+								</figcaption>
+							</figure>
+						</li>
+						<li>
+							<figure>
+								<img src="img/05-screenshot.jpg" alt="Screenshot 01">
+								<figcaption>
+								<div class="caption-content">
+									<a href="img/large/05.jpg" class="single_image">
 										<i class="fa fa-search"></i><br>
 										<p>Apresentação dos resultados</p>
 									</a>
